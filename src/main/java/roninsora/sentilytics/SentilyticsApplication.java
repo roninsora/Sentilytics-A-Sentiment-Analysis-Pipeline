67eecb4d17d246bbfbd972295ac2c644764501b2
roninsora <sohanoorrahmansourav@gmail.com>
2026-06-26 16:30:49 +0600

    initial commit

---
+++ src/main/java/roninsora/sentilytics/SentilyticsApplication.java
@@ -0,0 +1,13 @@
+package roninsora.sentilytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentilyticsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SentilyticsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/roninsora/sentilytics/SentilyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package roninsora.sentilytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SentilyticsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
